4b2160b252f721fb2db0917f7bd958d796418d44
Alfredo Luco <aluco@alumnos.uai.cl>
2015-09-24 16:25:03 -0300

    Initial Commit

---
+++ CommuteEvent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CommuteEvent
+//
+//  Created by Alfredo Luco on 24-09-15.
+//  Copyright (c) 2015 Alfredo Luco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
